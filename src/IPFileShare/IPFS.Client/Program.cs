c3ed092c39f7ee481894377ca7cb704f80f6ed1c
Mikhail <mikhails85@outlook.com>
2017-11-21 22:40:13 +0000

    Fix bugs in Runner

--- src/IPFileShare/IPFS.Client/Program.cs
+++ src/IPFileShare/IPFS.Client/Program.cs
@@ -2,7 +2,9 @@ using IPFS.Integration;
 using IPFS.Integration.Messages;
 using IPFS.Integration.Utils.Log;
 using System;
+using System.IO;
 using System.Threading.Tasks;
+using System.Threading;
 using IPFS.Runner;
 using IPFS.Results;
 
@@ -15,28 +17,31 @@ namespace IPFS.Client
         {
             Logger.SetupLogger(new ClientLogger());
             
-            var processResult = ProcessManager.CreateProcess("MyClient");     
+            var path = Path.GetFullPath("./.ipfs");
+                
+            Console.WriteLine(path);
+            
+            var processResult = await ProcessManager.StartProcess("MyClient", 
+                                    "/bin/bash","-c", $"\"IPFS_PATH={path} ipfs daemon\"" );     
             
             if(!processResult.Success)
             {
                 Console.WriteLine(processResult.Success ? "Success" : processResult.Errors[0].Message);
                 return;
             }
-            using(var process = processResult.Value)
-            {
-                process.OnExit = (exitResult) => { 
-                    Console.WriteLine(exitResult.Success ? "Success" : exitResult.Errors[0].Message);
-                };
-                process.Run( "./IPFS/ipfs.exe", "daemon");   
-               
-                var rest = new RESTClient("http://0.0.0.0:8081/");
             
-                var result = await rest.Message<DownloadFileMessage>().SendAsync("QmSwaM2teG1vXyhcmKGjAwkWw6vcYF948Yu2TSzFGEAjUZ");
+            ProcessManager.OnExit = (exitResult) => { 
+                Console.WriteLine(exitResult.Success ? "Success" : exitResult.Errors[0].Message);
+            };
             
-                Console.WriteLine(result.Success ? "Success" : result.Errors[0].Message);
+            var rest = new RESTClient("http://0.0.0.0:6001/");
+        
+            var result = await rest.Message<GetPeerInformation>().SendAsync();
+        
+            Console.WriteLine(result.Success ? "Success" : result.Errors[0].Message);
             
-                process.Stop();
-            }
+            ProcessManager.StopProcess();
+            
             Console.WriteLine("Done");
         }
     }
--- src/IPFileShare/IPFS.Runner/ProcessManager.cs
+++ src/IPFileShare/IPFS.Runner/ProcessManager.cs
@@ -1,29 +1,133 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using IPFS.Results;
-
+using IPFS.Runner.Errors;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
 namespace IPFS.Runner
 {
+    //https://blogs.msdn.microsoft.com/fkaduk/2017/02/22/using-strongly-typed-configuration-in-net-core-console-app/
     //http://www.levibotelho.com/development/async-processes-with-taskcompletionsource/
-    //https://medium.com/@dpursanov/running-python-script-from-c-and-working-with-the-results-843e68d230e5
-    //https://code.msdn.microsoft.com/windowsapps/Come-evitare-di-eseguire-3dfb6872
-    //https://ehikioya.com/multiple-instances-of-same-application/
-    //
     public static class ProcessManager
     {
-        public static Result<ProcessContext> CreateProcess(string appId)
-        {
-           var result = new Result<ProcessContext>();
-           var process = new ProcessContext(appId);
-           var canRunResult = process.CanRun();
-           if(!canRunResult.Success)
+        private static Process process;
+        private static Mutex mutex;
+        
+        public static Action<VoidResult> OnExit{ get; set; }
+        
+        public static async Task<VoidResult> StartProcess(string appId, string path, params string[] options)
+        {   
+           var result = new VoidResult();    
+           mutex = new Mutex(false, appId);
+           if (!mutex.WaitOne(TimeSpan.FromSeconds(0), false))
            {
-               result.AddErrors(canRunResult.Errors);
+               result.AddErrors(new ProcessAlreadyRunError());
                return result;
            }
-           result.SetValue(process);
-           return result;
+           
+           return await Starting(path, options);
         }
+        
+        public static void StopProcess()
+        {
+            if(process == null)
+            {
+                return;
+            }
+            
+            process.Kill ();
+		    process.Dispose ();
+			process = null;
+			
+            if(mutex == null)
+            {
+                return;
+            }
+            mutex.Dispose();
+            mutex = null;
+        }
+        
+        private static Task<VoidResult> Starting(string path, params string[] options)
+        {
+            var tcs = new TaskCompletionSource<VoidResult>();
+            
+            process = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo = CreateProcessStartInfo(path, options)
+            };
+            
+            var c = Console.Out;
+            
+            process.Exited += (sender, args) =>
+            {
+                ExitHandler();
+                
+                var result = new VoidResult();   
+                
+                var errorMessage = process.ExitCode != 0 ? process.StandardError.ReadToEnd():"Exit process";
+                result.AddErrors(new ProcessExitedWithError(errorMessage));
+                
+                tcs.SetResult(result);
+            };
+            
+            process.OutputDataReceived += (sender, e) =>
+			{
+			    Console.SetOut (c);
+				c.WriteLine(e.Data);
+				
+				if(e.Data.Contains("Daemon is ready"))
+				{
+				    var result = new VoidResult();    
+				    tcs.SetResult(result);
+				}
+			};
+			
+            process.Start();
+            
+            process.BeginOutputReadLine();
+			process.BeginErrorReadLine();
+			
+			return tcs.Task;
+        }
+        
+        private static void ExitHandler()
+        {
+            var exitResult = new VoidResult();
+                
+            if (process.ExitCode != 0)
+            {
+                var errorMessage = process.StandardError.ReadToEnd();
+                    
+                exitResult.AddErrors(new ProcessExitedWithError("The process did not exit correctly. " +
+                    "The corresponding error message was: " + errorMessage));    
+            }
+                
+            if(OnExit != null)
+            {
+                OnExit(exitResult);
+            }
+           
+            process.Dispose();
+            process = null;
+        }
+        
+        private static ProcessStartInfo CreateProcessStartInfo (string path, params string[] options)
+        {
+            var start = new ProcessStartInfo();
+            start.FileName = path;
+            start.Arguments = string.Join(" ", options);
+            start.UseShellExecute = false;
+            start.CreateNoWindow = true;
+            start.RedirectStandardOutput = true;
+            start.RedirectStandardError = true;
+            
+            return start;
+        }
+        
+        
     }
 }
