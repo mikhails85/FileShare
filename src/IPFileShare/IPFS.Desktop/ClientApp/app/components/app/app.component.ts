c2740e737b862256aa2bc456d0ecf9f10760e014
Mikhail <mikhails85@outlook.com>
2017-11-22 20:46:57 +0000

    Add navigation component, Fix layout bugs

--- src/IPFileShare/IPFS.Desktop/ClientApp/app/components/app/app.component.ts
+++ src/IPFileShare/IPFS.Desktop/ClientApp/app/components/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    public toggle : boolean = false;
+
+    public onManuToggle = ($event: boolean) => {
+       
+        this.toggle = $event;
+    }
 }
--- src/IPFileShare/IPFS.Desktop/ClientApp/app/components/navmenu/navmenu.component.ts
+++ src/IPFileShare/IPFS.Desktop/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
 
 @Component({
     selector: 'nav-menu',
     templateUrl: './navmenu.component.html',
-    styleUrls: ['./navmenu.component.css']
+//    styleUrls: ['./navmenu.component.css']
 })
 export class NavMenuComponent {
+    @Output() onManuToggle = new EventEmitter<boolean>();
+    public toggle : boolean = false;
+    
+    public ManuToggle = () => {
+        this.toggle = !this.toggle;
+        this.onManuToggle.emit(this.toggle)
+    }
 }
